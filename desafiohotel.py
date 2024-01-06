285505aa0150a07d1b74f549d1354d0f79bcc496
ThaG0592 <thaisg092@gmail.com>
2024-01-06 09:19:59 -0300

    Create desafiohotel.py
    Desenvolvimento do projeto Proz

---
+++ desafiohotel.py
@@ -0,0 +1,22 @@
+for numero in range(1, 21):
+    if numero != 13:
+        print(numero, end=" ")
+
+
+numero = 1
+while numero <= 20:
+    if numero != 13:
+        print(numero, end=" ")
+    numero += 1
+
+
+for numero in range(20, 0, -1):
+    if numero != 13:
+        print(numero, end=" ")
+
+
+numero = 20
+while numero >= 1:
+    if numero != 13:
+        print(numero, end=" ")
+    numero -= 1
