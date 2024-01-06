21e358a759651d255724ab5ed547f14f87557a30
ThaG0592 <thaisg092@gmail.com>
2024-01-06 09:23:51 -0300

    Update desafiohotel.py
    Acrescentei obsevação

--- desafiohotel.py
+++ desafiohotel.py
@@ -1,7 +1,10 @@
+# loop crescente com for
+
 for numero in range(1, 21):
     if numero != 13:
         print(numero, end=" ")
 
+# loop crescente com while
 
 numero = 1
 while numero <= 20:
@@ -10,10 +13,13 @@ while numero <= 20:
     numero += 1
 
 
+# loop decrescente com for
+
 for numero in range(20, 0, -1):
     if numero != 13:
         print(numero, end=" ")
 
+# loop decrescente com while
 
 numero = 20
 while numero >= 1:
